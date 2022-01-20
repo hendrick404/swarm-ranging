cbe48411ff622ff20497cf201f7b59a89754cf08
Hendrik Sauer <hendrik-sauer@mailbox.org>
2022-01-20 01:10:39 +0100

    Let antenna upload received messages

--- src/antenna/main.c
+++ src/antenna/main.c
@@ -11,11 +11,21 @@ LOG_MODULE_REGISTER(main);
 
 #define PAN_ID 0xDECA
 
+typedef enum { rx, tx } message_direction_t;
+
 typedef struct {
     uint16_t sender_id;
     uint16_t receiver_id;
     uint8_t sequence_number;
 } message_data_t;
+
+typedef struct {
+    uint16_t sender_id;
+    uint16_t receiver_id;
+    uint8_t sequence_number;
+    uint64_t timestamp;
+    message_direction_t mode;
+} message_info_t;
 
 static dwt_config_t config = {5, DWT_PRF_64M, DWT_PLEN_128, DWT_PAC8, 9, 9, 1, DWT_BR_6M8, DWT_PHRMODE_STD, (129)};
 
@@ -25,11 +35,33 @@ static uint8_t sequence_number = 0;
 
 static uint16_t id;
 
+uint64_t read_rx_timestamp() {
+    uint8_t timestamp_buffer[5];
+    dwt_readrxtimestamp(timestamp_buffer);
+    uint64_t timestamp = 0;
+    for (int i = 4; i >= 0; i--) {
+        timestamp <<= 8;
+        timestamp |= timestamp_buffer[i];
+    }
+    return timestamp;
+}
+
+uint64_t read_tx_timestamp() {
+    uint8_t timestamp_buffer[5];
+    dwt_readtxtimestamp(timestamp_buffer);
+    uint64_t timestamp = 0;
+    for (int i = 4; i >= 0; i--) {
+        timestamp <<= 8;
+        timestamp |= timestamp_buffer[i];
+    }
+    return timestamp;
+}
+
 /**
  * @brief Get the id object
- * 
+ *
  * This is a static part id to ranging id mapping.
- * 
+ *
  * @return uint16_t the id. If no mapping exists, 0 is returned as default.
  */
 uint16_t get_id() {
@@ -47,9 +79,14 @@ uint16_t get_id() {
     }
 }
 
+void process_message(message_info_t info) {
+    printk("{\"sender id\": %d, \"receiver id\": %d, \"sequence number\": %d, \"timestamp\": %lld}\n", info.sender_id,
+           info.receiver_id, info.sequence_number, info.timestamp);
+}
+
 /**
  * @brief Transmits a message.
- * 
+ *
  * @param data the data that should be trasmitted.
  * @return int 0 on successful transmission.
  */
@@ -81,7 +118,7 @@ int send_message(message_data_t data) {
 
 /**
  * @brief Checks if there are incoming messages and processes them
- * 
+ *
  */
 void check_received_messages() {
     uint32_t status = dwt_read32bitreg(SYS_STATUS_ID);
@@ -91,7 +128,17 @@ void check_received_messages() {
         LOG_INF("Frame length: %d", frame_length);
         uint8_t* rx_buffer = (uint8_t*) k_malloc(frame_length);
         dwt_readrxdata(rx_buffer, frame_length, 0);
-        LOG_HEXDUMP_INF(rx_buffer, frame_length - 2, "Received data");
+
+        message_info_t info;
+        info.mode = rx;
+        info.receiver_id = rx_buffer[6] << 8 | rx_buffer[5];
+        info.receiver_id = rx_buffer[8] << 8 | rx_buffer[7];
+        info.sequence_number = rx_buffer[2];
+        info.timestamp = read_rx_timestamp();
+        process_message(info);
+
+        LOG_HEXDUMP_DBG(rx_buffer, frame_length - 2, "Received data");
+
         k_free(rx_buffer);
         dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXFCG);
     } else if (status & SYS_STATUS_ALL_RX_ERR) {
@@ -102,7 +149,7 @@ void check_received_messages() {
 
 /**
  * @brief Checks if there are messages pending to be send and send them if so.
- * 
+ *
  */
 void check_message_to_send() {
     counter++;
@@ -122,7 +169,7 @@ int main(void) {
     reset_DW1000();
     port_set_dw1000_slowrate();
     openspi();
-    if (dwt_initialise(DWT_LOADNONE | DWT_READ_OTP_PID) == DWT_ERROR) {
+    if (dwt_initialise(DWT_LOADUCODE | DWT_READ_OTP_PID) == DWT_ERROR) {
         LOG_ERR("Failed to initialize dwm");
         return -1;
     }
