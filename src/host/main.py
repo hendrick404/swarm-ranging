4d383dd7a8a83cd044d6928e3a1105a1453d07a6
Hendrik Sauer <hendrik-sauer@mailbox.org>
2022-01-28 11:27:29 +0100

    Refactor

--- src/host/main.py
+++ src/host/main.py
@@ -3,32 +3,32 @@
 import json
 from typing import List, Optional, Tuple
 
-from serial import Serial, SerialException
+from serial import Serial
 
 
 class Node:
-    def __init__(self, id: int, serial_connection: Optional[Serial] = None):
-        self.id = id
-        self.serial_connection = serial_connection
+    def __init__(self, ranging_id: int, serial_connection: Optional[Serial] = None):
+        self.ranging_id: int = ranging_id
+        self.serial_connection: Optional[Serial] = serial_connection
+        self.distance: Optional[float] = None
 
     def set_distance(self, distance: float):
         self.distance = distance
 
-    def get_distance(self) -> float:
+    def get_distance(self) -> Optional[float]:
         return self.distance
 
     def get_serial_connection(self) -> Optional[Serial]:
         return self.serial_connection
 
-    def get_id(self) -> int:
-        return self.id
+    def get_ranging_id(self) -> int:
+        return self.ranging_id
 
 
 def main():
     """Main function. Loops forever and performs ranging."""
 
     connections: List[Node] = connect()
-    known_peers: List[Node] = []
 
     while True:
         for node in connections:
@@ -40,13 +40,13 @@ def main():
                     json_string = json_string[2 : len(json_string) - 3]  # TODO
                     decoded_json = json.JSONDecoder().decode(json_string)
                     if decoded_json:
-                        try:
-                            sender_id = decoded_json["range"]["sender_id"]
-                            print("Received message from " + str(sender_id))
-                        except KeyError:
-                            pass
+                        sender_id = decoded_json["range"]["sender_id"]
+                        print("Received message from " + str(sender_id))
                 except json.JSONDecodeError:
-                    pass  # Apparently we did not get a valid json
+                    # Apparently we did not get a valid json, maybe a debug log
+                    pass
+                except KeyError:
+                    pass
 
 
 def range_next() -> Optional[Tuple[Node, Node]]:
@@ -55,10 +55,11 @@ def range_next() -> Optional[Tuple[Node, Node]]:
 
 def connect() -> List[Node]:
     """Generates a list of connections."""
-    connections = []
-    connections.append(Node(1, Serial("/dev/tty.usbmodem0007601185891", 115200, timeout=1)))
-    connections.append(Node(2, Serial("/dev/tty.usbmodem0007601197931", 115200, timeout=1)))
-    connections.append(Node(3, Serial("/dev/tty.usbmodem0007601194831", 115200, timeout=1)))
+    connections = [
+        Node(1, Serial("/dev/tty.usbmodem0007601185891", 115200, timeout=1)),
+        Node(2, Serial("/dev/tty.usbmodem0007601197931", 115200, timeout=1)),
+        Node(3, Serial("/dev/tty.usbmodem0007601194831", 115200, timeout=1)),
+    ]
 
     return connections
 
