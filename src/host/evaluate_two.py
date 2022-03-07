cae1c3224217c8e1135b77063c81aa0e0f08ef9e
Hendrik Sauer <hendrik-sauer@mailbox.org>
2022-03-07 14:26:41 +0100

    Add simulation

--- src/host/evaluate_two.py
+++ src/host/evaluate_two.py
@@ -33,8 +33,10 @@ def main():
                                 rx_a = operation["rx range"]["rx time"]
                                 tx_b = operation["rx range"]["tx time"]
                                 rx_b = rx_timestamp["rx time"]
-                                print(f"Evaluating:\n\tTX_A: {tx_a}\n\tRX_A: {rx_a}\n\tTX_B: {tx_b}\n\tRX_B: {rx_b}\n")
-                                peer1_distance_measurements.append(measure_distance(tx_a, rx_a, tx_b, rx_b))
+                                (newest_seq_num, _) = peer1_transmissions[-1]
+                                if rx_a > tx_a and tx_b > rx_b and newest_seq_num - seq_num < 3:
+                                    print(f"Evaluating:\n\tTX_A: {tx_a}\n\tRX_A: {rx_a}\n\tTX_B: {tx_b}\n\tRX_B: {rx_b}\n")
+                                    peer1_distance_measurements.append(measure_distance(tx_a, rx_a, tx_b, rx_b))
         elif operation["id"] == peer2:
             if "tx range" in operation.keys():
                 peer2_transmissions.append((operation["tx range"]["seq num"], operation["tx range"]["tx time"]))
@@ -51,7 +53,7 @@ def main():
     print(peer2_distance_measurements)
 
 def measure_distance(tx_a, rx_a, tx_b, rx_b):
-    return ((rx_a - tx_a) - (tx_b - rx_b)) / 2 * speed_of_light * dwt_time_units
+    return ((rx_a - tx_a) - (tx_b - rx_b)) / 2 * dwt_time_units * speed_of_light 
 
 if __name__ == "__main__":
     main()
---
+++ src/host/simulate.py
@@ -0,0 +1,53 @@
+import json
+from math import sqrt
+from scipy.constants import speed_of_light
+
+class Node:
+    def __init__(self, id, pos, clock_err = 1, clock_offset = 0):
+        self.id = id
+        self.pos = pos
+        self.clock_err = clock_err
+        self.clock_offset = clock_offset
+        self.sequence_number = 1
+
+    def get_pos(self):
+        return self.pos
+
+    def tx(self, global_time):
+        json_obj = {"id": self.id, "tx range": {"seq num": self.sequence_number, "tx time": int(global_time * self.clock_err + self.clock_offset)}}
+        self.sequence_number += 1
+        return json_obj
+
+    def rx(self, global_time, message, pos):
+        rx_time = (global_time + (sqrt((self.pos[0] - pos[0]) ** 2 + (self.pos[1] + pos[1]) ** 2) / (speed_of_light / 1_000_000_000_000))) * self.clock_err + self.clock_offset
+        json_obj = {"id": self.id, "rx range": {"sender id": message["id"], "seq num": message["tx range"]["seq num"], "tx time": message["tx range"]["tx time"], "rx time": int(rx_time)}}
+        return json_obj
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+def main():
+    eval_file = open("evaluation_simulated.txt", "a")
+    exchange_counter = 1
+    ranging_interval = 1_000_000_000_000
+    global_clock = 0
+    nodes: List[Node] = [Node(1,(0,0)), Node(2,(60,0))]
+    while exchange_counter <= 100 * len(nodes):
+        for tx_node in nodes:
+            global_clock += ranging_interval / len(nodes)
+            tx_message = tx_node.tx(global_clock)
+            eval_file.write(json.JSONEncoder().encode(tx_message) + "\n")
+            for rx_node in nodes:
+                if rx_node != tx_node:
+                    rx_message = rx_node.rx(global_clock, tx_message, tx_node.get_pos())
+                    eval_file.write(json.JSONEncoder().encode(rx_message) + "\n")
+        exchange_counter += 1
+
+
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
